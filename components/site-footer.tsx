34366a5b1e044ee1423c91b1af3480b1dccf917d
Ofentse Lengwasa <151065301+ofentsebotlhale@users.noreply.github.com>
2026-06-28 14:02:42 +0200

    refactor: update footer social links and icons
    Replaced existing social links with updated accounts for WhatsApp, Facebook, and Instagram and added corresponding SVG icons.

--- components/site-footer.tsx
+++ components/site-footer.tsx
@@ -6,8 +6,44 @@ import { useRef } from 'react'
 
 const SOCIALS = [
   {
+    label: 'WhatsApp',
+    href: 'https://wa.me/message/IYQSC46QIM5BP1',
+    icon: (props: any) => (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        {...props}
+      >
+        <path d="M21 11.5a8.38 8.38 0 0 1-.9 3.8 8.5 8.5 0 0 1-7.6 4.7 8.38 8.38 0 0 1-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 0 1-.9-3.8 8.5 8.5 0 0 1 4.7-7.6 8.38 8.38 0 0 1 3.8-.9h.5a8.48 8.48 0 0 1 8 8v.5z" />
+      </svg>
+    )
+  },
+  {
+    label: 'Facebook',
+    href: 'https://www.facebook.com/share/1PKDMxQTLx/?mibextid=wwXIfr',
+    icon: (props: any) => (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        {...props}
+      >
+        <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
+      </svg>
+    )
+  },
+  {
     label: 'Instagram',
-    href: 'https://instagram.com',
+    href: 'https://www.instagram.com/obxstudio_/',
     icon: (props: any) => (
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -26,26 +62,8 @@ const SOCIALS = [
     )
   },
   {
-    label: 'X / Twitter',
-    href: 'https://x.com',
-    icon: (props: any) => (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        {...props}
-      >
-        <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z" />
-      </svg>
-    )
-  },
-  {
     label: 'LinkedIn',
-    href: 'https://linkedin.com',
+    href: 'https://www.linkedin.com/company/obxstudio/',
     icon: (props: any) => (
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -60,27 +78,6 @@ const SOCIALS = [
         <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
         <rect width="4" height="12" x="2" y="9" />
         <circle cx="4" cy="4" r="2" />
-      </svg>
-    )
-  },
-  {
-    label: 'Dribbble',
-    href: 'https://dribbble.com',
-    icon: (props: any) => (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        {...props}
-      >
-        <circle cx="12" cy="12" r="10" />
-        <path d="M19.13 5.09C15.22 9.14 10 10.44 2.25 10.94" />
-        <path d="M21.75 12.84c-6.62-1.41-12.14 1-16.38 6.32" />
-        <path d="M8.56 2.75c4.37 6 6 9.42 8 17.72" />
       </svg>
     )
   },
